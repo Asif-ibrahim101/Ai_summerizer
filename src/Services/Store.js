59f8d9e297ab8249e934ad4bd31bc4b790ede678
Asif Ibrahim <127242567+Asif-ibrahim101@users.noreply.github.com>
2023-11-15 23:54:30 +0000

    Updated Store.js

--- src/Services/Store.js
+++ src/Services/Store.js
@@ -1,9 +1,10 @@
-import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+
 import { articleApi } from "./Article";
 
 export const store = configureStore({
     reducer: {
-        [articleApi.reducerPath] : articleApi.reducer
+        [articleApi.reducerPath]: articleApi.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
-});
+})
