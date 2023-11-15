d6df970769a3fbb7207a6ea72844bc7ce079957b
Asif Ibrahim <127242567+Asif-ibrahim101@users.noreply.github.com>
2023-11-15 23:53:10 +0000

    Updated Article.js
    Changed the code for Article.js and Store.js because there was a bug in the code

--- src/Services/Article.js
+++ src/Services/Article.js
@@ -1,37 +1,25 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-//     params: {
-//         url: 'https://time.com/6266679/musk-ai-open-letter/',
-//         length: '3'
-//     },
-//     headers: {
-//         'X-RapidAPI-Key': 'f02bb763b7mshb59b0b23f56f68ap18e5c5jsnbd4c4910e7aa',
-//         'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-//     }
-// };
-
-const RapidAPIKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'ttps://article-extractor-and-summarizer.p.rapidapi.com/',
-        prepareHeaders: (Headers) => {
-            Headers.set('X-RapidAPI-Key', RapidAPIKey);
-            Headers.set('X-RapidAPI-Host', 
-            'article-extractor-and-summarizer.p.rapidapi.com');
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
-            return Headers;
-        }
+            return headers;
+        },
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
-        })
-    })
-});
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
 
-export const {useLazyGetSummaryQuery} = articleApi;
+export const { useLazyGetSummaryQuery } = articleApi
